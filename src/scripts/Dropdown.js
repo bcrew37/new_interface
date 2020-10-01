8c9b5fc6369d82c2644a3b2b59b0141031243097
Mike <vloger34355@gmail.com>
2020-10-01 18:05:00 +0300

    commit

--- src/scripts/Dropdown.js
+++ src/scripts/Dropdown.js
@@ -1,31 +1,30 @@
+
 (function (Factory) {
 
     class Dropdown {
         constructor() {
-            document.querySelectorAll(".drop-down").forEach(node => {
-                let btn = node.querySelector('[data-event="toggle"]')
+            document.querySelectorAll(".drop-down").forEach(n => {
+                let btn = n.querySelector('[data-event="toggle"]')
                 if (btn) {
-                    btn.addEventListener("click", () => (!node.classList.contains("active")) ? this.open(node) : this.close(node))
-                    window.addEventListener("click", (e) => {
-                        let target = e.target.closest(".drop-down")
-                        if (target) { return } else this.close(node)
-                    })
+                    btn.addEventListener("click", (e) => (!n.classList.contains("active")) ? this.open(n) : this.close(n))
                 }
+                window.addEventListener("click", e => {
+                    if (e.target.closest(".drop-down")) { return } else this.close(n)
+                })
             })
+
         }
 
-        open(node) {
-            let menu = node.querySelector(".drop-down_menu")
-            menu.style.display = "block"
-            menu.style.opacity = 1
-            node.classList.add("active")
+        open(n) {
+            let m = n.querySelector(".drop-down_menu")
+            if (m) { m.style.display = "block"; m.style.opacity = 1; }
+            n.classList.add("active")
         }
 
-        close(node) {
-            let menu = node.querySelector(".drop-down_menu")
-            menu.style.opacity = 0
-            menu.style.display = "none"
-            node.classList.remove("active")
+        close(n) {
+            let m = n.querySelector(".drop-down_menu")
+            if (m) { m.style.opacity = 0; m.style.display = "none"; }
+            n.classList.remove("active")
         }
     }
 
